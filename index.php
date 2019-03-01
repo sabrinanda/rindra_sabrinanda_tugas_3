89fc541e2952b000910f1ecf63f76791e72f5b8e
sabrinanda <rindraarfi@gmail.com>
2019-03-01 16:39:22 +0700

    tambah

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas PHP</title>
+</head>
+<body>
+	<table style="border: 1px; padding: 3px; border-color: black;">
+		<tr>
+            <th>No</th>
+            <th>Nama Barang</th>
+            <th>Jumlah</th>
+			<th>Harga</th>
+        </tr>
+         <?php
+			$i=1;
+           {
+        ?>
+        <tr>
+            <td><?php echo 1;?></td>
+            <td><?php echo 2;?></td>
+			<td><?php echo 3;?></td>
+			<td><?php echo 4;?></td>
+        </tr>
+         <tr>
+            <td><?php echo 1;?></td>
+            <td><?php echo 2;?></td>
+			<td><?php echo 3;?></td>
+			<td><?php echo 4;?></td>
+        </tr>
+        <?php $i++;} ?>
+	</table>
+</body>
+</html>
